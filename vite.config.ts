878dc92f2edc224739632fee7235e388f6322d1c
Omar Barguti <omarbarguti@microsoft.com>
2025-08-15 23:34:56 -0400

    Fix scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',          // so "@/styles/..." works
+      '@styles': '/src/styles',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // prepended to EVERY scss file automatically
+        additionalData: `@use "@styles/index" as *;`,
+      },
+    },
+  },
+});
